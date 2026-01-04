4149d83387be17512a9e6428d860ca32ca0b49e7
Milad Shahriyari <shahriyarimilad30@gmail.com>
2026-01-04 14:41:01 +0330

    reduce reviwed for js

--- sceipt.js
+++ sceipt.js
@@ -23,7 +23,7 @@
 //filter  یک ارایه برمیگرداند
 //find یک ایتم برمیگرداند
 
-const array = [22, 3, 43, 65, 7];
+// const array = [22, 3, 43, 65, 7];
 
 // const newNumber = array.filter(function (item) {
 //   return item > 10;
@@ -31,5 +31,15 @@ const array = [22, 3, 43, 65, 7];
 // ---or
 // const newNumber = array.filter((item) => item > 10);
 // ----find
-const newNumber = array.find((item) => item === 65);
-console.log(newNumber);
+// const newNumber = array.find((item) => item === 65);
+// console.log(newNumber);
+
+//----------------------
+// reduce
+
+const number = [2, 3, 5, 6, 7];
+const result = number.reduce(
+  (accumulator, currentItem) => accumulator + currentItem,
+  0
+);
+console.log(result);
