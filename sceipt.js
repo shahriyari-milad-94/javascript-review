7b687592f2e3d4127ec61ed259a72078b5e63e26
Milad Shahriyari <shahriyarimilad30@gmail.com>
2026-01-04 13:54:41 +0330

    map reviewed for js

--- sceipt.js
+++ sceipt.js
@@ -4,11 +4,16 @@
 // }
 // console.log(duplicate(5));
 // ---------------------------------------------
-const duplicate = (number1, number2) => {
-  const newNumer1 = number1 + 2;
-  const newNumer2 = number2 + 3;
-  const result = newNumer1 + newNumer2;
-  return result;
-};
+// const duplicate = (number) => number*2
 
-console.log(duplicate(5, 4));
+// console.log(duplicate(5));
+// ------------------------------------------------------
+//map
+
+const array = [1, 2, 3];
+const newArray = array.map(function (item) {
+  const result = item * 2;
+  return result;
+});
+
+console.log(newArray);
