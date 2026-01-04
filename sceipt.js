7069d19c117d2c6c259513e4d996fa86969ca8c1
Milad Shahriyari <shahriyarimilad30@gmail.com>
2026-01-04 13:44:21 +0330

    arrow function reviewed for js

--- sceipt.js
+++ sceipt.js
@@ -1,1 +1,14 @@
-console.log("ali");
+// function duplicate(nubmer) {
+//   const result = number * 2;
+//   return result;
+// }
+// console.log(duplicate(5));
+// ---------------------------------------------
+const duplicate = (number1, number2) => {
+  const newNumer1 = number1 + 2;
+  const newNumer2 = number2 + 3;
+  const result = newNumer1 + newNumer2;
+  return result;
+};
+
+console.log(duplicate(5, 4));
